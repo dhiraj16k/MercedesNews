8774f6606e11905a021dc68b7b7b09f01d496d61
dhiraj16k <dhiraj.kumar@daimler.com>
2022-01-11 19:23:55 +0530

    bookmark fixed

---
+++ app/src/main/java/com/example/mercedesnews/fragments/ProfileFragment.kt
@@ -0,0 +1,61 @@
+package com.example.mercedesnews.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.mercedesnews.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.example.mercedesnews.*
+import com.example.mercedesnews.databinding.FragmentProfileBinding
+import kotlinx.android.synthetic.main.fragment_profile.*
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        binding.logoutBtn.setOnClickListener{
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null){
+            val email = firebaseUser.email
+            binding.emailTv.text = email
+        }
+        else{
+            val intent = Intent (activity, MainActivity::class.java)
+            activity?.startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
